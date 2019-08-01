0a34a1e49e0d8e5c4fff072f16e20ab491dce7ed
Rafael Santana <jrafaeldesantana@gmail.com>
2019-08-01 16:47:14 -0300

    refresh token method

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -30,7 +30,16 @@ class AuthController {
   }
 
   async refresh({ request, response, auth }) {
-    //
+    var refresh_token = request.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    const user = await auth
+      .newRefreshToken()
+      .generateForRefreshToken(refresh_token)
+    return response.send({ data: user })
   }
 
   async logout({ request, response, auth }) {
