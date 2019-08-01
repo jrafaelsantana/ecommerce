adcca247d788c06fc807d63bcb166e9228ca8a73
Rafael Santana <jrafaeldesantana@gmail.com>
2019-08-01 19:50:16 -0300

    list product controller

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Product = use('App/Models/Product')
+
 /**
  * Resourceful controller for interacting with products
  */
@@ -15,9 +17,18 @@ class ProductController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagination }) {
+    const name = request.input('name')
+    const query = Product.query()
+
+    if (name) {
+      query.where('name', 'LIKE', `%${name}%`)
+    }
+
+    const products = await query.pagination(pagination.page, pagination.limit)
+    return response.send(products)
+  }
 
   /**
    * Create/save a new product.
