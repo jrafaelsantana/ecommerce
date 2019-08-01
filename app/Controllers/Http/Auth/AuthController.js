97d5f9f0e1c9a686c725349302d7f5cfcd83541c
Rafael Santana <jrafaeldesantana@gmail.com>
2019-08-01 16:43:06 -0300

    login method

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -23,7 +23,10 @@ class AuthController {
   }
 
   async login({ request, response, auth }) {
-    //
+    const { email, password } = request.all()
+
+    let data = await auth.withRefreshToken().attempt(email, password)
+    return response.send({ data })
   }
 
   async refresh({ request, response, auth }) {
