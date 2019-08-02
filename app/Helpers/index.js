f5636428a7a70419e8a3f7d4185166e12037e0fc
Rafael Santana <jrafaeldesantana@gmail.com>
2019-08-01 21:25:30 -0300

    manage multiple uploads helper

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -29,8 +29,10 @@ const str_random = async (length = 40) => {
 /**
  * Move um único arquivo para o caminho especificado, se nenhum for especificado
  * então 'public/uploads' será utilizado.
+ *
  * @param { FileJar } file o arquivo a ser gerenciado
  * @param { string } path o caminho para onde o arquivo deve ser movido
+ * @return { Object<FileJar> }
  */
 const manage_single_upload = async (file, path = null) => {
   path = path ? path : Helpers.publicPath('uploads')
@@ -45,6 +47,40 @@ const manage_single_upload = async (file, path = null) => {
   return file
 }
 
+/**
+ *  Move um multiplos arquivos para o caminho especificado, se nenhum for especificado
+ * então 'public/uploads' será utilizado.
+ *
+ * @param { FileJar } file os arquivos a serem gerenciados
+ * @param { string } path o caminho para onde os arquivos devem ser movidos
+ * @return {Object}
+ */
+const manage_multiple_uploads = async (fileJar, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+  let successes = [],
+    errors = []
+
+  await Promise.all(
+    fileJar.files.map(async file => {
+      let random_name = await str_random(30)
+      let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+      await file.move(path, {
+        name: filename
+      })
+
+      if (file.moved()) {
+        successes.push(file)
+      } else {
+        errors.push(file.errors())
+      }
+    })
+  )
+  return { successes, errors }
+}
+
 module.exports = {
-  str_random
+  str_random,
+  manage_single_upload,
+  manage_multiple_uploads
 }
